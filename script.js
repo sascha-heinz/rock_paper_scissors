8182f509a88e3859de9e5d507c206f69c0fedb38
sascha-heinz <177232819+sascha-heinz@users.noreply.github.com>
2025-04-17 19:55:28 +0200

    commenting out most of the code experimenting with EventListener setting up dependencies and the basic sequence of the functions

--- script.js
+++ script.js
@@ -1,153 +1,155 @@
-//putting all the game into a function, so the game only starts, when i ask for it by ckliking the start button
-//function game() {
-
-//Clear Console
-//console.clear();
-
-//Define global variables (especially needed for counting the score)
-let playerScore = 0;
-let computerScore = 0;
-let tieScore = 0;
-let round = 1;
 
 
-
-
-/* überlegungen zum Aufbau
-1) globale Vriablen festlegen (Global)
-2) for Schleife 5x für
-    2.1) getComputerChoice (Funktion)
-    2.2) getPlayerChoice ()
-        2.2.1) EventListener buttons (Global)
-        2.2.2) 
-
-
-
-
-
-
-
-function getPlayerChoice() {
-    let playersChoice
-    const nodeListButtons = document.querySelectorAll("button")
-    nodeListButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            playersChoice = button.textContent;
-        });
-    });
-
-    return playersChoice
-}
 /*
 
 
-//Loop to start game and to play the game 5 rounds by calling the functions and count the score
-for (i = 1; i<=5; i++) {
+let playerChoice
+const nodeListButtons = document.querySelectorAll("button")
+
+
+
+
+
+
+
+//putting all the game into a function, so the game only starts, when i ask for it by ckliking the start button
+function game() {
+
+    //Clear Console
+    console.clear();
     
-    //calling the functions and write the return (result) in a variable (const)
-    const player = getPlayerChoice();
-    const computer = getComputerChoice();
-    const winner = getWinner(computer, player);
+    //Define global variables (especially needed for counting the score)
+    let playerScore = 0;
+    let computerScore = 0;
+    let tieScore = 0;
+    let round = 1;
 
-    //counting the score after the outcome of a round is clear
-    if (winner === "player won") {
-        playerScore++
-    }
-    else if (winner === "computer won") {
-        computerScore++
-    }
-    else if (winner === "tie") {
-        tieScore++
-    }
-    else {}
+    */
 
-    //write the result of the round in the console
-    console.log("-----------------------------");
-    console.log("Player: " + player);
-    console.log("Computer: " + computer);
-    console.log("Round " + i + "/5" + ": " + winner);
+    
+    
+    //Loop to start game and to play the game 5 rounds by calling the functions and count the score
+   // for (i = 1; i<=5; i++) {
+
+    let playerChoice
+    let computerChoice
+
+
+   const nodeListButtons = document.querySelectorAll("button")
+        nodeListButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                playerChoice = button.textContent;
+                console.log(playerChoice)
+                playTheRound();
+            });
+        }); 
+  //  }
+ 
+
+    function playTheRound() {
+
+        computerChoice = getComputerChoice();
+        
+
+    }
+
+
+    function getComputerChoice(){
+        const choices = ["Rock", "Paper", "Scissor"];
+        return choices[Math.floor(Math.random()*3)];
+    }
+ 
+ 
+ 
+ 
+ 
+ 
+        /*       //calling the functions and write the return (result) in a variable (const)
+        const player = getPlayerChoice();
+        const computer = getComputerChoice();
+        const winner = getWinner(computer, player);
+    
+        //counting the score after the outcome of a round is clear
+        if (winner === "player won") {
+            playerScore++
+        }
+        else if (winner === "computer won") {
+            computerScore++
+        }
+        else if (winner === "tie") {
+            tieScore++
+        }
+        else {}
+    
+        //write the result of the round in the console
+        console.log("-----------------------------");
+        console.log("Player: " + player);
+        console.log("Computer: " + computer);
+        console.log("Round " + i + "/5" + ": " + winner);
+        console.log("Score: " + playerScore + ":" + computerScore);
+        console.log("-----------------------------");
+    
+        //which round are we actually in?
+        round++
+    }
+    
+    //write the result of the game in the console and ask for a new game
+    console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
+    console.log("End of game")
     console.log("Score: " + playerScore + ":" + computerScore);
-    console.log("-----------------------------");
-
-    //which round are we actually in?
-    round++
-}
-
-//write the result of the game in the console and ask for a new game
-console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
-console.log("End of game")
-console.log("Score: " + playerScore + ":" + computerScore);
-if (playerScore > computerScore) {
-    console.log("Congratulations, you won!")
-}
-else if (playerScore < computerScore) {
-    console.log("This one you lost. Try again!")
-}
-else {
-    console.log("That's a tie. Try again!")
-}
-
-console.log(`Click on the "Start" button for a new game`);
-console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
-
-//Function to ask for the players choice and convert choice to lower Case (to make it more versatile in case of different spelling)
-function getPlayerChoice () {   
-    let answer = prompt("Round " + round + "/5" + ": " + 'Please enter your weapon (rock, paper or scissor)');
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you won!")
+    }
+    else if (playerScore < computerScore) {
+        console.log("This one you lost. Try again!")
+    }
+    else {
+        console.log("That's a tie. Try again!")
+    }
     
-    if (answer !== null) {
-        answer = answer.toLowerCase()
-        if (answer !== "rock" && answer !== "paper" && answer !== "scissor") {   
-            console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");  
-            console.log("you better check your grammar");     
-            console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");    
-            return getPlayerChoice();
-            }
+    console.log(`Click on the "Start" button for a new game`);
+    console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
+    
+    //Function to ask for the players choice and convert choice to lower Case (to make it more versatile in case of different spelling)
+    function getPlayerChoice () {   
+        nodeListButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                playerChoice = button.textContent;
+            });
+        });
+        return playerChoice
+    }
+    
+    //Function to randomly choose between rock, paper and scissor for the computer
+       function getComputerChoice () {
+        let randomNumber = Math.random()*100;
+         if (randomNumber <= 33) {
+            return "rock";
+         }
+    
+         else if (randomNumber >= 66) {
+            return "paper";
+         }
+    
+         else {
+            return "scissor"
+         }
+    
+       }
+    
+    //Function to compare players choice and computers choice an decide, who's the winner
+    function getWinner (computer, player) {
+        if (computer === player) {
+            return "tie"
+        }
+    
+        else if (computer === "rock" && player === "paper" || computer === "paper" && player === "scissor" || computer === "scissor" && player === "rock") {
+            return "player won"
+        }
     
         else {
-            return answer
+            return "computer won"
         }
     }
     
-    else {
-        console.log("Are you serious??????????????????????????")
-        console.log("You better not cancel. That's a point for the computer")
-        console.log("Are you serious??????????????????????????")
-        return "pathetic"
-    }
-}
-
-//Function to randomly choose between rock, paper and scissor for the computer
-   function getComputerChoice () {
-    let randomNumber = Math.random()*100;
-     if (randomNumber <= 33) {
-        return "rock";
-     }
-
-     else if (randomNumber >= 66) {
-        return "paper";
-     }
-
-     else {
-        return "scissor"
-     }
-
-   }
-
-//Function to compare players choice and computers choice an decide, who's the winner
-function getWinner (computer, player) {
-    if (computer === player) {
-        return "tie"
-    }
-
-    else if (computer === "rock" && player === "paper" || computer === "paper" && player === "scissor" || computer === "scissor" && player === "rock") {
-        return "player won"
-    }
-
-    else {
-        return "computer won"
-    }
-}
-
-}
-
-*/
+    }  */
