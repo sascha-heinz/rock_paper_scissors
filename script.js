f9783833d884f12d8cd2af9f6ff9f51747fcb30d
sascha-heinz <177232819+sascha-heinz@users.noreply.github.com>
2025-04-17 22:34:05 +0200

    setting up tetx, structure and logic for the last round

--- script.js
+++ script.js
@@ -1,44 +1,10 @@
-
-
-/*
-
-
-let playerChoice
-const nodeListButtons = document.querySelectorAll("button")
-
-
-
-
-
-
-
-//putting all the game into a function, so the game only starts, when i ask for it by ckliking the start button
-function game() {
-
-    //Clear Console
-    console.clear();
-    
-    //Define global variables (especially needed for counting the score)
-    let playerScore = 0;
-    let computerScore = 0;
-    let tieScore = 0;
-    let round = 1;
-
-    */
-
-    
-    
-    //Loop to start game and to play the game 5 rounds by calling the functions and count the score
-   // for (i = 1; i<=5; i++) {
-
     let playerChoice;
     let computerChoice;
     let winner;
-    let rounds = 1;
+    let round = 1;
     const maxRounds = 5;
     let playerScore = 0;
     let computerScore = 0;
-    let tieScore = 0;
 
 
 
@@ -47,14 +13,25 @@ function game() {
         
    nodeListButtons.forEach((button) => {
             button.addEventListener("click", () => {
-                if (rounds <= maxRounds) {
-                    playerChoice = button.textContent;
-                    console.log(playerChoice);
-                    playTheRound();
+                playerChoice = button.textContent;
+                if (playerChoice === "Reset") {
+                    reset();
                 }
                 else {
-                    console.log("End")
+                    if (round < maxRounds) {
+                        console.log(playerChoice);
+                        playTheRound();
+                    }
+
+                    else if (round === maxRounds) {
+                        console.log(playerChoice)
+                        playTheRound();
+                        result(playerScore, computerScore);
                 }
+                    else {
+                        console.log('End of game. For new Game, please click "Reset"');
+                    }
+                }          
             });
         }); 
   //  }
@@ -64,7 +41,7 @@ function game() {
         computerChoice = getComputerChoice();
         console.log(computerChoice)
         winner = getWinner(playerChoice, computerChoice);
-        rounds++
+        round++
     }
 
 
@@ -81,104 +58,40 @@ function game() {
                  playerChoice === "Paper" && computerChoice === "Rock" || 
                  playerChoice === "Scissor" && computerChoice === "Paper" ) {
                     console.log("won")
+                    playerScore++
                  }  
         else {
             console.log("lost")
-        }
-
-
-    }
- 
-
- 
- 
- 
-        /*       //calling the functions and write the return (result) in a variable (const)
-        const player = getPlayerChoice();
-        const computer = getComputerChoice();
-        const winner = getWinner(computer, player);
-    
-        //counting the score after the outcome of a round is clear
-        if (winner === "player won") {
-            playerScore++
-        }
-        else if (winner === "computer won") {
             computerScore++
         }
-        else if (winner === "tie") {
-            tieScore++
+
+
+    }
+
+    function result(playerScore, computerScore) {
+        if (playerScore === computerScore) {
+            console.log("That's a tie. Try again!")
         }
-        else {}
-    
-        //write the result of the round in the console
-        console.log("-----------------------------");
-        console.log("Player: " + player);
-        console.log("Computer: " + computer);
-        console.log("Round " + i + "/5" + ": " + winner);
-        console.log("Score: " + playerScore + ":" + computerScore);
-        console.log("-----------------------------");
-    
-        //which round are we actually in?
-        round++
-    }
-    
-    //write the result of the game in the console and ask for a new game
-    console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
-    console.log("End of game")
-    console.log("Score: " + playerScore + ":" + computerScore);
-    if (playerScore > computerScore) {
-        console.log("Congratulations, you won!")
-    }
-    else if (playerScore < computerScore) {
-        console.log("This one you lost. Try again!")
-    }
-    else {
-        console.log("That's a tie. Try again!")
-    }
-    
-    console.log(`Click on the "Start" button for a new game`);
-    console.log("<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>");
-    
-    //Function to ask for the players choice and convert choice to lower Case (to make it more versatile in case of different spelling)
-    function getPlayerChoice () {   
-        nodeListButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-                playerChoice = button.textContent;
-            });
-        });
-        return playerChoice
-    }
-    
-    //Function to randomly choose between rock, paper and scissor for the computer
-       function getComputerChoice () {
-        let randomNumber = Math.random()*100;
-         if (randomNumber <= 33) {
-            return "rock";
-         }
-    
-         else if (randomNumber >= 66) {
-            return "paper";
-         }
-    
-         else {
-            return "scissor"
-         }
-    
-       }
-    
-    //Function to compare players choice and computers choice an decide, who's the winner
-    function getWinner (computer, player) {
-        if (computer === player) {
-            return "tie"
+        else if (playerScore < computerScore) {
+            console.log("Unfortunately (literally unfortunately) you lost. Try again! May the force be with you.")
         }
-    
-        else if (computer === "rock" && player === "paper" || computer === "paper" && player === "scissor" || computer === "scissor" && player === "rock") {
-            return "player won"
-        }
-    
         else {
-            return "computer won"
+            console.log("Congratulations! You won!")
         }
     }
-    
-    }  */
+
+    function reset() {
+        playerChoice;
+        computerChoice;
+        winner;
+        round = 1;
+        playerScore = 0;
+        computerScore = 0;
+        console.clear();
+    }
+ 
+
+ 
+/* Es muss noch eine Funktion eingefügt werden, die genau die letzte Runde beschreibt (rounds === 5). Es soll in einem Zug die letzte Runde gespielt werden
+   und "Spiel Ende: y hat gewonnen" ausgegeben werden. Bei einem nochmaligen klicken soll nur darauf verwiesen werdn, dass das Spiel neugetsartet werden muss.
+*/
