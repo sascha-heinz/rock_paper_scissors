68d74279ebe06873ab728a5833d56be5692c9796
sascha-heinz <177232819+sascha-heinz@users.noreply.github.com>
2025-04-25 21:24:02 +0200

    inject text from js to the html. set up logic for several games. easter egg. push to main.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+    let WarLordCount = 0;
+    let LasVegasScore = 0;
+    let DonQuijoteScore = 0;
     let playerChoice;
     let computerChoice;
     let winner;
@@ -5,45 +8,65 @@
     const maxRounds = 5;
     let playerScore = 0;
     let computerScore = 0;
+    const nodeListButtons = document.querySelectorAll("button");
+    const outputDiv = document.getElementById("game-output");
+    const BattleOutputParagraph = document.createElement("p");
+    const WarOutputParagraph = document.createElement("p");
+    const ResetParagraph = document.createElement("p")
+    const LasVegasParagraph = document.createElement("p")
+      
 
-
-
-
-   const nodeListButtons = document.querySelectorAll("button")
-        
    nodeListButtons.forEach((button) => {
             button.addEventListener("click", () => {
                 playerChoice = button.textContent;
-                if (playerChoice === "Reset") {
+                if (playerChoice === "Reset" && WarLordCount >= 50) {
+                    window.close();
+                }
+                else if (playerChoice === "Reset"){
                     reset();
                 }
                 else {
                     if (round < maxRounds) {
-                        console.log(playerChoice);
                         playTheRound();
                     }
 
                     else if (round === maxRounds) {
-                        console.log(playerChoice)
                         playTheRound();
                         result(playerScore, computerScore);
+
                 }
-                    else {
-                        console.log('End of game. For new Game, please click "Reset"');
+                    else if (round > maxRounds && WarLordCount < 50){
+                        ResetParagraph.innerHTML = `Click "Reset" for a new game.`;
+                        outputDiv.appendChild(ResetParagraph);
+                        console.log('End of game. For new game, please click "Reset".');
                     }
                 }          
             });
         }); 
-  //  }
  
 
     function playTheRound() {
         computerChoice = getComputerChoice();
-        console.log(computerChoice)
-        winner = getWinner(playerChoice, computerChoice);
-        round++
-    }
+        winner = getWinner(playerChoice, computerChoice);    
+        BattleOutputParagraph.innerHTML = `-----------------------------<br>
+                                    Round ${round}<br>
+                                    Player: ${playerChoice}<br>
+                                    Computer: ${computerChoice}<br>
+                                    Battle Outcome: ${winner}<br>
+                                    Battle Score: ${playerScore}:${computerScore}<br>
+                                    -----------------------------`;
+        outputDiv.appendChild(BattleOutputParagraph);
 
+        console.log("--------------------------");
+        console.log("Round " + round);
+        console.log("Players choice: " + playerChoice);
+        console.log("Computers choice: " + computerChoice);
+        console.log("Battle Outcome: " + winner)
+        console.log("Battle Score: " + playerScore + ":" + computerScore)
+        console.log("--------------------------")
+
+        round++;
+    }
 
     function getComputerChoice(){
         const choices = ["Rock", "Paper", "Scissor"];
@@ -52,31 +75,64 @@
 
     function getWinner(playerChoice, computerChoice) {
         if (playerChoice === computerChoice) {
-            console.log("tie")
+            return "Standoff"
         }
         else if (playerChoice === "Rock" && computerChoice === "Scissor" || 
                  playerChoice === "Paper" && computerChoice === "Rock" || 
                  playerChoice === "Scissor" && computerChoice === "Paper" ) {
-                    console.log("won")
                     playerScore++
+                    return "Advantage"
                  }  
         else {
-            console.log("lost")
-            computerScore++
+            computerScore++;
+            return "Reversal";
         }
 
 
     }
 
     function result(playerScore, computerScore) {
+        
+        WarLordCount++;
+
         if (playerScore === computerScore) {
-            console.log("That's a tie. Try again!")
+            console.log("Deadlock. Try again!");
+            WarOutputParagraph.innerHTML = "Deadlock. Try again!";
+            
         }
         else if (playerScore < computerScore) {
-            console.log("Unfortunately (literally unfortunately) you lost. Try again! May the force be with you.")
+            console.log("Defeat. May the force be with you next time.");
+            WarOutputParagraph.innerHTML = "Defeat. May the force be with you next time.";
+            DonQuijoteScore++;
+
         }
         else {
-            console.log("Congratulations! You won!")
+            console.log("Glorious Victory!")
+            WarOutputParagraph.innerHTML = "Glorious Victory!";
+            LasVegasScore++;
+        }
+
+        outputDiv.appendChild(WarOutputParagraph);
+
+
+        if (WarLordCount >= 10 && WarLordCount < 50) {
+            if (LasVegasScore > 0 && LasVegasScore >= DonQuijoteScore * 5) {
+                LasVegasParagraph.innerHTML = "I don't recommend you going to Las Vegas. With that luck, they will just unalive you. I recommend you to travel the world an earn your money by gambling here and there. Or just start a company. It will go well, trust me";
+            }
+                else if (LasVegasScore > 0 && LasVegasScore >= DonQuijoteScore * 3 && LasVegasScore < DonQuijoteScore * 5) {
+                    LasVegasParagraph.innerHTML = "At this point, you could make gambling your main income source. Las Vegas is a great place to be!";
+                }
+                else if (LasVegasScore > 0 && LasVegasScore < DonQuijoteScore * 3 && LasVegasScore >= DonQuijoteScore * 2) {
+                    LasVegasParagraph.innerHTML = "You really should think about spending a weekend in Las Vegas!";
+                }
+                else {
+                    LasVegasParagraph.innerHTML = "";
+                }
+                outputDiv.appendChild(LasVegasParagraph)      
+        }
+        if (WarLordCount >= 50) {
+            LasVegasParagraph.innerHTML = "You really should visit an therapist or sth. Better an psychiatrist.";
+            outputDiv.appendChild(LasVegasParagraph);
         }
     }
 
@@ -88,10 +144,6 @@
         playerScore = 0;
         computerScore = 0;
         console.clear();
-    }
- 
+        outputDiv.innerHTML = "";
 
- 
-/* Es muss noch eine Funktion eingefügt werden, die genau die letzte Runde beschreibt (rounds === 5). Es soll in einem Zug die letzte Runde gespielt werden
-   und "Spiel Ende: y hat gewonnen" ausgegeben werden. Bei einem nochmaligen klicken soll nur darauf verwiesen werdn, dass das Spiel neugetsartet werden muss.
-*/
+    }
